6510aa436079f7369dc9d7d3cef2a740af6c0c91
Wei Gao <jky239@gmail.com>
2014-02-01 21:01:35 +0800

    save description to other files

---
+++ README.md
@@ -0,0 +1,15 @@
+# JsDoc i18n Plugin
+
+JsDoc i18n plugin is to provide i18n translates for jsdocs.
+
+
+## Installation
+
+Install with npm:
+
+    npm install jsdoc-i18n-plugins --save
+    
+    
+## Usage
+
+
--- index.js
+++ index.js
@@ -12,27 +12,73 @@ var directory = conf.directory || "./locales",
 // make absolute path for directory
 directory = path.resolve(cwd, directory);
 
+directory = path.join(directory, conf.locale || 'en_US');
+
+if(!fs.existsSync(directory))
+    fs.mkPath(directory);
+
+
 // normalize extension
 (extension[0] !== ".") && (extension = '.' + extension);
 
-
+var localesData = {};
 
 exports.handlers = {
     newDoclet: function(e) {
         // e.doclet will refer to the newly created doclet
         // you can read and modify properties of that doclet if you wish
-
-        if (typeof e.doclet.description === 'string') {
-            console.log(e.doclet.description);
-
-            var filename = e.doclet.meta.filename,
-                mc = /(.*)(\.(:?[^.]+)?)$/.exec(filename);
+        var desc = e.doclet.description;
+        if (typeof desc  === 'string') {
+            var key = getLocaleFile(path.join(e.doclet.meta.path, e.doclet.meta.filename)),
+                data = localesData[key];
             
-            if(mc) filename = mc[1];
-            
-            var localeFile = path.join(directory, path.relative(srcDir, e.doclet.meta.path), 
-                                       filename + extension);
-            console.log(localeFile);
+            if(data.hasOwnProperty(desc)) {
+                if(desc != data[desc])
+                    e.doclet.description = desc;
+            }else {
+                data[desc] = desc;
+            }
+        }else {
+            // undefined
+        }
+    },
+    fileBegin: function(e) {
+        var key = getLocaleFile(e.filename), data;
+        if(fs.existsSync(key)) {
+            try {
+                data = JSON.parse(fs.readFileSync(key));
+            }catch(e) {
+                console.warn(e);
+            }
+        }
+        
+        localesData[key] = data || {};
+    },
+    fileComplete: function(e) {
+        var key = getLocaleFile(e.filename),
+            data = localesData[key];
+        if(data) {
+            var dir  = path.dirname(key);
+            if(!fs.existsSync(dir)) {
+                fs.mkPath(dir);
+            }
+
+            fs.writeFileSync(key, JSON.stringify(data, null, 4), 'utf8');
         }
     }
 };
+
+
+function getLocaleFile(filename) {
+    mc = /(.*)(\.(:?[^.]+)?)$/.exec(filename);
+    
+    if(mc) filename = mc[1];
+    
+    var localeFile = path.join(directory, path.relative(srcDir, path.dirname(filename)), 
+                               path.basename(filename) + extension);
+    return localeFile;
+}
+
+
+
+
